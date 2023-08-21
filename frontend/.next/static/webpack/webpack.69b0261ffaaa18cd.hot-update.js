09ddcf7a6a70d63b54fae435a82e460a2adfa7e6
aecgdh <86180934+aecgdh@users.noreply.github.com>
2023-08-21 12:06:44 +0530

    newest

--- frontend/.next/static/webpack/webpack.69b0261ffaaa18cd.hot-update.js
+++ frontend/.next/static/webpack/webpack.69b0261ffaaa18cd.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "8528e20f615803eb"; }
+/******/ 	__webpack_require__.h = function() { return "0520bf4d9b695e05"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
